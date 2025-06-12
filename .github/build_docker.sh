1e5829fe82e94b8b84188254b188e975c599a9ec
pcmagas <pcmagas@disroot.org>
2025-06-12 22:09:37 +0300

    Release Into Ghcr as well.

--- .github/build_docker.sh
+++ .github/build_docker.sh
@@ -5,5 +5,15 @@ docker build -t pcmagas/fedora-rpmpkg .
 BRANCH=${GITHUB_REF##*/}
 
 if [[ $BRANCH == 'master' ]]; then
+
+    GHCR_IMAGE="ghcr.io/${GITHUB_REPOSITORY,,}:latest"
+    docker tag pcmagas/fedora-rpmpkg "$GHCR_IMAGE"
+    
+    # Authenticate with GitHub Container Registry
+    echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
+
+    # Push to GHCR
+    docker push "$GHCR_IMAGE"
+
     docker image push pcmagas/fedora-rpmpkg:latest
 fi
