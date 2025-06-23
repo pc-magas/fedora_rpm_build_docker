1b5f5fcbf5f3d29e6e90294a16a22a2191e132c8
pcmagas <pcmagas@disroot.org>
2025-06-23 23:55:47 +0300

    Fix permissions for all directories under rpmbuild.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 usermod -u ${UID} pkgbuild
 groupmod -g ${GID} pkgbuild
 
-chown -R pkgbuild:pkgbuild /home/pkgbuild/rpmbuild/RPMS
-chown -R pkgbuild:pkgbuild /home/pkgbuild/rpmbuild/SRPMS
+chown -R pkgbuild:pkgbuild /home/pkgbuild/rpmbuild
 chown -R pkgbuild:pkgbuild /home/pkgbuild/code
 
 echo $COPR_SECRET > /home/pkgbuild/.config/copr
